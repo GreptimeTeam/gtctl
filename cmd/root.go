89f9cc23b281ca499ac8e4a08ebd536f55727045
zyy17 <zyylsxm@gmail.com>
2022-11-09 15:08:53 +0800

    feat: add test banner(looks more cool)

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 
 	"github.com/GreptimeTeam/gtctl/cmd/app/create"
@@ -11,12 +13,14 @@ import (
 	internalversion "github.com/GreptimeTeam/gtctl/pkg/version"
 )
 
+const gtctlTextBanner = "          __       __  __\n   ____ _/ /______/ /_/ /\n  / __ `/ __/ ___/ __/ / \n / /_/ / /_/ /__/ /_/ /  \n \\__, /\\__/\\___/\\__/_/   \n/____/   \n"
+
 func NewRootCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Args:    cobra.NoArgs,
 		Use:     "gtctl",
 		Short:   "gtctl is a command-line tool for managing GreptimeDB cluster.",
-		Long:    `gtctl is a command-line tool for managing GreptimeDB cluster.`,
+		Long:    fmt.Sprintf("%s\ngtctl is a command-line tool for managing GreptimeDB cluster.", gtctlTextBanner),
 		Version: internalversion.Get().String(),
 	}
 
