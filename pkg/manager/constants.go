3b8816f94e6026c3e078d340a1d440d8772b77f4
zyy17 <zyylsxm@gmail.com>
2022-11-15 12:17:42 +0800

    refactor: pull artifacts from remote charts (#27)
    * refactor: pull artifacts from remote charts
    
    * refactor: modify default operator version

--- pkg/manager/constants.go
+++ pkg/manager/constants.go
@@ -1,11 +1,7 @@
 package manager
 
 const (
-	defaultOperatorReleaseName = "greptimedb-operator"
-
+	defaultOperatorReleaseName       = "greptimedb-operator"
 	defaultOperatorHelmPackageName   = "greptimedb-operator"
 	defaultGreptimeDBHelmPackageName = "greptimedb"
-
-	defaultOperatorHelmPackageVersion   = "0.1.0-alpha.2"
-	defaultGreptimeDBHelmPackageVersion = "0.1.0"
 )
