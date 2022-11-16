08378a8fc768c2c8d4be765074ddafcafa281d5f
liyang <ly18846162402@163.com>
2022-11-16 11:54:57 +0800

    refactor: del show gtctl version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -11,39 +11,32 @@ var (
 	buildDate  = "none"
 )
 
-const (
-	gtctlVersion = "0.1.0"
-)
-
 type Version struct {
-	GtctlVersion string
-	GitCommit    string
-	GitVersion   string
-	GoVersion    string
-	Compiler     string
-	Platform     string
-	BuildDate    string
+	GitCommit  string
+	GitVersion string
+	GoVersion  string
+	Compiler   string
+	Platform   string
+	BuildDate  string
 }
 
 func (v Version) String() string {
-	format := "GtctlVersion: %s\n" +
-		"GitCommit: %s\n" +
+	format := "GitCommit: %s\n" +
 		"GitVersion: %s\n" +
 		"GoVersion: %s\n" +
 		"Compiler: %s\n" +
 		"Platform: %s\n" +
 		"BuildDate: %s\n"
-	return fmt.Sprintf(format, v.GtctlVersion, v.GitCommit, v.GitVersion, v.GoVersion, v.Compiler, v.Platform, v.BuildDate)
+	return fmt.Sprintf(format, v.GitCommit, v.GitVersion, v.GoVersion, v.Compiler, v.Platform, v.BuildDate)
 }
 
 func Get() Version {
 	return Version{
-		GtctlVersion: gtctlVersion,
-		GitCommit:    gitCommit,
-		GitVersion:   gitVersion,
-		GoVersion:    runtime.Version(),
-		Compiler:     runtime.Compiler,
-		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
-		BuildDate:    buildDate,
+		GitCommit:  gitCommit,
+		GitVersion: gitVersion,
+		GoVersion:  runtime.Version(),
+		Compiler:   runtime.Compiler,
+		Platform:   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		BuildDate:  buildDate,
 	}
 }
