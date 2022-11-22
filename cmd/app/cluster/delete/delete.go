2cc0fe1cb241062acc11ecc51775cd86dd2a9ec8
liyang <ly18846162402@163.com>
2022-11-22 19:25:00 +0800

    refactor: delete the etcd detach from the greptimedb

--- cmd/app/cluster/delete/delete.go
+++ cmd/app/cluster/delete/delete.go
@@ -3,6 +3,7 @@ package delete
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -29,7 +30,6 @@ func NewDeleteClusterCommand(l log.Logger) *cobra.Command {
 			}
 
 			clusterName, namespace := args[0], options.Namespace
-			clusterExist := true
 			l.Infof("⚠️ Deleting cluster '%s' in namespace '%s'...\n", log.Bold(clusterName), log.Bold(namespace))
 
 			m, err := manager.New(l, false)
@@ -44,24 +44,21 @@ func NewDeleteClusterCommand(l log.Logger) *cobra.Command {
 			})
 			if errors.IsNotFound(err) {
 				l.Infof("Cluster '%s' in '%s' not found\n", clusterName, namespace)
-				clusterExist = false
+				return nil
 			} else if err != nil && !errors.IsNotFound(err) {
 				return err
 			}
 
-			etcdNamespace := cluster.Spec.Meta.EtcdEndpoints[0]
-
-			if clusterExist {
-				if err := m.DeleteCluster(ctx, &manager.DeleteClusterOption{
-					ClusterName: clusterName,
-					Namespace:   options.Namespace,
-				}); err != nil {
-					return err
-				}
-
-				// TODO(zyy17): Should we wait until the cluster is actually deleted?
-				l.Infof("Cluster '%s' in namespace '%s' is deleted!\n", clusterName, namespace)
+			etcdNamespace := strings.Split(strings.Split(cluster.Spec.Meta.EtcdEndpoints[0], ".")[1], ":")[0]
+			if err := m.DeleteCluster(ctx, &manager.DeleteClusterOption{
+				ClusterName: clusterName,
+				Namespace:   options.Namespace,
+			}); err != nil {
+				return err
 			}
+
+			// TODO(zyy17): Should we wait until the cluster is actually deleted?
+			l.Infof("Cluster '%s' in namespace '%s' is deleted!\n", clusterName, namespace)
 
 			if options.TearDownEtcd {
 				l.Infof("⚠️ Deleting etcd cluster in namespace '%s'...\n", log.Bold(etcdNamespace))
