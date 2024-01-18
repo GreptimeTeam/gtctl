1a87a1762f1d8d2fff3432f7aea16a095cfde75f
liyang <ly18846162402@163.com>
2024-01-18 11:14:16 +0800

    chore: format license header

--- hack/version.sh
+++ hack/version.sh
@@ -13,17 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-
-
-
-
-
-
-
-
-
 function ldflag() {
     local key=${1}
     local val=${2}
